63badc225aea32b56f6521f79edcc8e87ea409a2
Garcia Romo Josue <42949680+lilRomo@users.noreply.github.com>
2020-05-18 16:25:03 -0500

    Direccion amigos

--- scripts/autorizacion.js
+++ scripts/autorizacion.js
@@ -1,123 +1,160 @@
-auth.onAuthStateChanged( user => {
+auth.onAuthStateChanged( user =>{
+ 
     if(user){
+        console.log('Usuario entró');
 
-        db.collection('platillos').onSnapshot(snapshot =>{
-            obtienePlatillos(snapshot.docs);
+        if(navigator.geolocation){
+
+            navigator.geolocation.getCurrentPosition( position =>{
+                
+
+                db.collection('usuarios').doc(user.uid).update({
+                    coordenadas : {
+                        latitude : position.coords.latitude,
+                        longitude : position.coords.longitude
+                    }
+                });
+
+            });
+        }
+
+
+        db.collection('usuarios').onSnapshot(snapshot =>{
+            obtieneAmigos(snapshot.docs);
+            configuraMenu(user);
+        }, err => {
+            console.log(err.message);
         });
-        configurarMenu(user);
+
+
+        var name, email, photoUrl, uid, emailVerified;
+
+        name = user.displayName;
+        email = user.email;
+        photoUrl = user.photoURL;
+        emailVerified = user.emailVerified;
+        uid = user.uid;  
+        
+        console.log(name,email,photoUrl,emailVerified,uid);
+
+    }
+    else{
+        console.log('Usuario salió');
+        obtieneAmigos([]);
+        configuraMenu();
     }
 
-    else{
-        obtienePlatillos([]);
-        configurarMenu();
-    }
 });
 
-const formaingresar = document.getElementById('formaingresar');
 
-formaingresar.addEventListener('submit', (e)=>{
+const formaregistrate = document.getElementById('formaregistrate');
+
+formaregistrate.addEventListener('submit',(e)=>{
     e.preventDefault();
 
-    let correo = formaingresar['correo'].value;
-    let contrasena = formaingresar['contrasena'].value;
+    const correo = formaregistrate['rcorreo'].value;
+    const contrasena = formaregistrate['rcontrasena'].value;
 
-    auth.signInWithEmailAndPassword(correo,contrasena).then( cred => {
-        console.log(cred)
+    auth.createUserWithEmailAndPassword(correo,contrasena).then( cred =>{
 
-        $('#ingresarmodal').modal('hide');
-        formaingresar.reset();
-        formaingresar.querySelector('.error').innerHTML='';
+        return db.collection('usuarios').doc(cred.user.uid).set({
+            nombre: formaregistrate['rnombre'].value,
+            telefono: formaregistrate['rtelefono'].value,
+            direccion: formaregistrate['rdireccion'].value
+        });
 
-    }).catch(err => {
-        formaingresar.querySelector('.error').innerHTML=mensajeError(err.code);
-        console.log(err)
+
+    }).then( ()=>{
+        $('#registratemodal').modal('hide');
+        formaregistrate.reset();
+        formaregistrate.querySelector('.error').innerHTML = '';
+    }).catch( err => {
+        formaregistrate.querySelector('.error').innerHTML = mensajeError(err.code);
     });
+
+
 });
 
-
-function mensajeError(codigo){
-    let mensaje = '';
-    switch(codigo){
-        case 'auth/wrong-password':
-            mensaje = 'Su contraseña no es correcta'
-            break;
-
-        case 'auth/user-not-found':
-            mensaje = 'Usuario no encontrado'
-            break;
-
-        case 'auth/weak-password':
-            mensaje = 'Contraseña Debil'
-            break;
-        default:
-            mensaje = 'Ocurrio un error al ingresar con este usuario'
-    }
-
-    return mensaje;
-}
 
 const salir = document.getElementById('salir');
 
-salir.addEventListener('click', (e) =>{
-
-e.preventDefault();
-auth.signOut().then(() => {
-    alert('El usuario cerro sesion');
-});
-
-});
-
-
-const formaregistrarte = document.getElementById('formaregistrate');
-
-formaregistrarte.addEventListener('submit', (e)=>{
+salir.addEventListener('click', (e)=>{
     e.preventDefault();
+    auth.signOut().then(()=>{
+        alert("El usuario ha salido del sistema");
+    });
+
+});
+
+
+function mensajeError(codigo) {
+
+    let mensaje = '';
+
+    switch(codigo) {
+        case 'auth/wrong-password':
+          mensaje = 'Su contraseña no es correcta';
+          break;
+        case 'auth/user-not-found':
+            mensaje = 'El usuario no existe o el correo no esta registrado';
+            break;
+        case 'auth/weak-password':
+            mensaje = 'Contraseña débil debe tener al menos 6 caracteres';
+            break;
+        default:
+            mensaje = 'Ocurrió un error al ingresar con este usuario';
+      }
+    return mensaje;
+  }
+
+const formaingresar =  document.getElementById('formaingresar');
+
+formaingresar.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let correo = formaingresar['correo'].value;
+    let contrasena = formaingresar['contrasena'].value;
+
+    auth.signInWithEmailAndPassword(correo,contrasena).then( cred =>{
+
+        $('#ingresarmodal').modal('hide');
+        formaingresar.reset();
+        formaingresar.querySelector('.error').innerHTML = '';
+    }).catch( err => {
+
+        formaingresar.querySelector('.error').innerHTML = mensajeError(err.code);
+        console.log(err);
+    });
     
-    const correo = formaregistrarte['rcorreo'].value;
-    const contrasena = formaregistrarte['rcontrasena'].value;
-
-    auth.createUserWithEmailAndPassword(correo,contrasena).then(cred =>{
-       
-       return db.collection('usuarios').doc(cred.user.uid).set({
-            nombre: formaregistrarte['rnombre'].value,
-            telefono:formaregistrarte['rtelefono'].value,
-            direccion:formaregistrarte['rdireccion'].value
-        });
-
-        
-    }).then(()=>{
-        $('#registratemodal').modal('hide');
-        formaregistrarte.reset();
-        formaregistrarte.querySelector('.error').innerHTML = '';
-
-    }).catch(err => {
-        formaregistrarte.querySelector('.error').innerHTML = mensajeError(erro);
-
-    });;
-    
-})
+});
 
 
-entrarGoogle = () =>{
+entrarGoogle = () => {
+ 
     var provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithPopup(provider).then(function(result){
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+
         var token = result.credential.accessToken;
         console.log(token);
 
         var user = result.user;
 
-        let html = `
-            <p>Nombre: ${user.displayName} </p>
-            <p>Correo: ${user.email} </p>
-            <img src="${user.photoURL}">
-        `;
+            console.log(user);
+            const html = `
+                <p>Nombre: ${ user.displayName }</p>
+                <p>Correo: ${ user.email}</p>
+                <img src="${ user.photoURL }" width="50px">
+            `;
+            datosdelacuenta.innerHTML = html;
 
-        datosdelacuenta.innerHTML = html;
-        $('#ingresarmodal').modal('hide');
-        formaingresar.reset();
-        formaingresar.querySelector('.error').innerHTML = '';
-    }).catch( function(error){
+            $('#ingresarmodal').modal('hide');
+            formaingresar.reset();
+            formaingresar.querySelector('.error').innerHTML = '';
+
+
+        // ...
+        }).catch(function(error) {
             console.log(error);
     });
+
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,72 +1,61 @@
-const listaloggedout = document.querySelectorAll('.logged-out')
-const listaloggedin = document.querySelectorAll('.logged-in')
+const listaloggedout = document.querySelectorAll('.logged-out');
+const listaloggedin = document.querySelectorAll('.logged-in');
 const datosdelacuenta = document.querySelector('.datosdelacuenta');
 
-const configurarMenu = (user) => {
+const configuraMenu = (user) => {
     if(user){
-        db.collection('usuarios').doc(user.uid).get().then(doc =>{
-            const html =  `
-                <p>Nombre: ${doc.data().nombre } </p>
-                <p>Correo: ${user.email } </p>
-                <p>Nombre: ${doc.data().telefono } </p>
-                <p>Nombre: ${doc.data().direccion } </p>
+       
 
-            
-            
-                `;
+       db.collection('usuarios').doc(user.uid).get().then( doc =>{
+           const html = `
+               <p>Nombre: ${ doc.data().nombre }</p>
+               <p>Correo: ${ user.email}</p>
+               <p>Teléfono: ${ doc.data().telefono }</p>
+               <p>Dirección: ${ doc.data().direccion }</p>
+               <p>Coordenadas: ${ doc.data().coordenadas.latitude } , ${ doc.data().coordenadas.longitude }</p>
+           `;
+           datosdelacuenta.innerHTML = html;
+       });
 
-            datosdelacuenta.innerHTML = html;
-        });
-        listaloggedin.forEach( item => item.style.display = 'block');
-        listaloggedout.forEach( item => item.style.display = 'none');
-    }else{
-
-        
-        listaloggedin.forEach( item => item.style.display = 'none');
-        
-        listaloggedout.forEach( item => item.style.display = 'block');
-
+       listaloggedin.forEach( item => item.style.display = 'block');
+       listaloggedout.forEach( item => item.style.display = 'none');
     }
-};
-
-
-const lsitadeplatillos = document.getElementById('listadeplatillos');
-
-const obtienePlatillos = (data) => {
-
-    if(data.length){
-
-        let html = '';
-        data.forEach(doc =>{
-            const platillo = doc.data();
-            const columna = `
-
-
-            
-            
-            <div class="col-12 col-md-4 ">
-
-            <br>
-            <br>
-            <br>
-                <img src="images/${platillo.imagen}" alt="${platillo.nombre}">
-                <p class="text-white">${platillo.nombre}</p>
-                <p class="text-danger">$${platillo.precio}</p>
-                <a href="https://www.paypal.me/josueromo/${platillo.precio}" target="_blank">
-                    <button class="btn btn-primary">Pagar Ahora</button>
-                </a>
-            </div>
-            `;
-
-            html += columna;
-        });
-
-        lsitadeplatillos.innerHTML = html;
+    else
+    {
+       datosdelacuenta.innerHTML = '';
+       listaloggedin.forEach( item => item.style.display = 'none');
+       listaloggedout.forEach( item => item.style.display = 'block');
     }
-    else{
-        lsitadeplatillos.innerHTML = '<img src="https://images.squarespace-cdn.com/content/5a61f37ae45a7c70210b916b/1516973560749-9ZBMVC78835AP9VQJOOU/Welcome.png?format=750w&content-type=image%2Fpng" alt="Italian Trulli">'
-    }
+}
+
+const obtieneAmigos = (data) =>{
+
+   var propiedades = { 
+       center: { 
+                   lat: 21.152639, lng: -101.711598 
+               }, 
+       zoom: 14 
+   }
+
+   var mapa =  document.getElementById("map")
+   var map = new google.maps.Map(mapa, propiedades);
+
+
+   data.forEach( doc => {
+       
+       informacion = new google.maps.InfoWindow;
+
+       var pos = { 
+           lat: doc.data().coordenadas.latitude,
+           lng: doc.data().coordenadas.longitude
+       };
+
+       informacion.setPosition(pos);
+       informacion.setContent(doc.data().nombre);
+       informacion.open(map);
+
+   });
+
+
 
 };
-
-
